c51940d574fd6f3a51b47a616f4d90c9b4fcfb2f
antham <hamonanth@gmail.com>
2013-08-03 19:38:49 +0200

    First commit

---
+++ app/js/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('yacy', ['yacy.filters', 'yacy.services', 'yacy.directives', 'yacy.controllers']).
+  config(['$routeProvider', function($routeProvider) {
+  }]);
