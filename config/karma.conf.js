eb3ca829c5ba0638cec488101915cb94e66b103c
antham <hamonanth@gmail.com>
2013-08-05 01:17:32 +0200

    Remove useless tests, update karma config

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,13 +1,18 @@
 basePath = '../';
 
 files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'app/lib/angular/angular.js',
-  'app/lib/angular/angular-*.js',
-  'test/lib/angular/angular-mocks.js',
-  'app/js/**/*.js',
-  'test/unit/**/*.js'
+    JASMINE,
+    JASMINE_ADAPTER,
+    "app/bower_components/angular/angular.min.js",
+    "app/bower_components/angular-ui/build/angular-ui-ieshiv.min.js",
+    "app/bower_components/angular-ui/build/angular-ui.min.js",
+    "app/bower_components/angular-bootstrap/ui-bootstrap.min.js",
+    "app/bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js",
+    "app/bower_components/angular-cookies/angular-cookies.min.js",
+    "app/bower_components/angular-localStorage/src/localFactory.js",
+    'test/lib/angular/angular-mocks.js',
+    'app/js/**/*.js',
+    'test/unit/**/*.js'
 ];
 
 autoWatch = true;
@@ -15,6 +20,6 @@ autoWatch = true;
 browsers = ['Chrome'];
 
 junitReporter = {
-  outputFile: 'test_out/unit.xml',
-  suite: 'unit'
+    outputFile: 'test_out/unit.xml',
+    suite: 'unit'
 };
