edbddaa94da94bca148181f6efa541bf23951c33
antham <hamonanth@gmail.com>
2014-01-15 18:46:13 +0100

    Add bower.json, remove vendor folder

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,6 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
-angular.module('yacy', ['ui.bootstrap', 'localStorage', 'yacy.filters', 'yacy.services', 'yacy.directives', 'yacy.controllers']).
-    config(['$routeProvider', function($routeProvider) {
+angular.module('yacy', ['ngRoute', 'ui.bootstrap', 'ngResource', 'ngCookies', 'angularLocalStorage', 'yacy.filters', 'yacy.services', 'yacy.directives', 'yacy.controllers']).
+  config(['$routeProvider', function($routeProvider) {
     }]);
