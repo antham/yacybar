47a6249eae06e37b1def980ba8a93c1bbbba3cc4
antham <hamonanth@gmail.com>
2014-02-04 00:16:02 +0100

    Fix indent

--- app/js/services.js
+++ app/js/services.js
@@ -3,38 +3,38 @@
 /* Services */
 
 angular.module('yacy.services', []).
-    factory('chrome', function() {
-        return chrome;
-    }).
-    factory('storage', function() {
-        var Storage = function() {
-        };
+  factory('chrome', function() {
+    return chrome;
+  }).
+  factory('storage', function() {
+    var Storage = function() {
+    };
 
-        Storage.prototype = {
-            set : function(key,value)
-            {
-                localStorage.setItem(key,angular.toJson(value));
-            },
-            get : function(key)
-            {
-                return localStorage.getItem(key) == 'undefined' ? undefined : angular.fromJson(localStorage.getItem(key));
-            },
-            remove : function(key)
-            {
-                localStorage.removeItem(key);
-            },
-            has : function(key)
-            {
-                return localStorage.getItem(key) == null ? false : true;
-            }
-        }
+    Storage.prototype = {
+      set : function(key,value)
+      {
+        localStorage.setItem(key,angular.toJson(value));
+      },
+      get : function(key)
+      {
+        return localStorage.getItem(key) == 'undefined' ? undefined : angular.fromJson(localStorage.getItem(key));
+      },
+      remove : function(key)
+      {
+        localStorage.removeItem(key);
+      },
+      has : function(key)
+      {
+        return localStorage.getItem(key) == null ? false : true;
+      }
+    }
 
-        return new Storage();
-    }).
-    factory('uri', function() {
-        return {
-            new: function(args) {
-                return new URI(args);
-            }
-        };
+    return new Storage();
+  }).
+  factory('uri', function() {
+    return {
+      new: function(args) {
+        return new URI(args);
+      }
+    };
 
